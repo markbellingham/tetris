a39ea71748e3dda664e629a7af356fbad298c660
Mark Bellingham <mark.bellingham@protonmail.ch>
2020-05-29 09:07:35 +0100

    * Fixed bug where tetrominoes don't redraw properly on multi-line score  * Scores are higher for multi-line scores  * Changed styling to centre on screen, add grid lines

--- app.js
+++ app.js
@@ -213,12 +213,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add score
     function addScore() {
+        let multiple = 0;
         for(let i = 0; i < 199; i+=width) {
             const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9];
 
             if(row.every( index => squares[index].classList.contains('taken'))) {
-                score += 10;
+                score += 10 + multiple;
+                multiple += 5;
                 scoreDisplay.innerHTML = score;
+                undraw();
                 row.forEach( index => {
                     squares[index].classList.remove('taken', 'tetromino');
                     squares[index].style.backgroundColor = '';
@@ -226,6 +229,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const squaresRemoved = squares.splice(i, width);
                 squares = squaresRemoved.concat(squares);
                 squares.forEach( cell => grid.appendChild(cell) );
+                draw();
             }
         }
     }
